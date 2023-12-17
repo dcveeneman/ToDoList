14cf49debd50bbd22854548944e562023f2ed558
dcveeneman <143367653+dcveeneman@users.noreply.github.com>
2023-12-17 15:39:18 -0800

    2023/12/17 - Chapter 5.3: Mic-chapter commit as 6:15 in video

--- ToDoList/DetailView.swift
+++ ToDoList/DetailView.swift
@@ -9,13 +9,29 @@ import SwiftUI
 
 struct DetailView: View {
     @Environment(\.dismiss) private var dismiss
+    @State private var toDo = ""
+    @State private var reminderIsOn = false
+    @State private var dueDate = Date.now + (60*60*24)
+    @State private var notes = ""
+
     var passedValue: String
     
     var body: some View {
         List {
-            
+            TextField("Enter To Do here", text: $toDo)
+                .font(.title)
+                .textFieldStyle(.roundedBorder)
+                .padding(.vertical)
+                .listRowSeparator(.hidden)
+            Toggle("Set Reminder:", isOn: $reminderIsOn)
+                .padding(.top)
+                .listRowSeparator(.hidden)
+           DatePicker("Date", selection: $dueDate)
+                .padding(.top)
+                .listRowSeparator(.hidden)
+            TextField("Notes", text: $notes, axis: .vertical)
         }
-
+        .listStyle(.plain)
         .padding()
     }
 }
