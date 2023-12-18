2869c55b67b5ac092893ca3c39fe2f90c9c327d2
dcveeneman <143367653+dcveeneman@users.noreply.github.com>
2023-12-18 13:55:34 -0800

    2023/12/18 - Chapter 5.3: Mid-chapter commit as 12:15 in video

--- ToDoList/DetailView.swift
+++ ToDoList/DetailView.swift
@@ -13,26 +13,50 @@ struct DetailView: View {
     @State private var reminderIsOn = false
     @State private var dueDate = Date.now + (60*60*24)
     @State private var notes = ""
+    @State private var isCompleted = false
 
     var passedValue: String
     
     var body: some View {
-        List {
-            TextField("Enter To Do here", text: $toDo)
-                .font(.title)
-                .textFieldStyle(.roundedBorder)
-                .padding(.vertical)
-                .listRowSeparator(.hidden)
-            Toggle("Set Reminder:", isOn: $reminderIsOn)
-                .padding(.top)
-                .listRowSeparator(.hidden)
-           DatePicker("Date", selection: $dueDate)
-                .padding(.top)
-                .listRowSeparator(.hidden)
-            TextField("Notes", text: $notes, axis: .vertical)
+        NavigationStack {
+            List {
+                TextField("Enter To Do here", text: $toDo)
+                    .font(.title)
+                    .textFieldStyle(.roundedBorder)
+                    .padding(.vertical)
+                    .listRowSeparator(.hidden)
+                Toggle("Set Reminder:", isOn: $reminderIsOn)
+                    .padding(.top)
+                    .listRowSeparator(.hidden)
+                DatePicker("Date", selection: $dueDate)
+                    .disabled(!reminderIsOn)
+                    .listRowSeparator(.hidden)
+                    .padding(.bottom)
+                Text ("Notes:")
+                    .padding(.top)
+                TextField("", text: $notes, axis: .vertical)
+                    .textFieldStyle(.roundedBorder)
+                    .listRowSeparator(.hidden)
+                Toggle ("Completed:", isOn: $isCompleted)
+                    .padding(.top)
+                    .listRowSeparator(.hidden)
+            }
+            .listStyle(.plain)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button("Cancel") {
+                        dismiss()
+                    }
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Save") {
+                        // TODO: Add Save code here
+                    }
+                }
+            }
+            .navigationBarBackButtonHidden(true)
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .listStyle(.plain)
-        .padding()
     }
 }
 
