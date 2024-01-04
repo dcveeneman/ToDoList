970426a4147b2cecc240d8fbb05236ab3c885ffb
dcveeneman <143367653+dcveeneman@users.noreply.github.com>
2024-01-03 17:17:06 -0800

    2024/01/03 - Chapter 5.6: Stopped at 19:45 in video

---
+++ ToDoList/ToDo.swift
@@ -0,0 +1,8 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by David Veeneman on 1/3/24.
+//
+
+import Foundation
---
+++ ToDoList/ToDoViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ToDoViewModel.swift
+//  ToDoList
+//
+//  Created by David Veeneman on 1/3/24.
+//
+
+import Foundation
